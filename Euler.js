6f85265b7cf06c2a27cb3775bbd42ed0e0087df2
Ohranj <67558167+Ohranj@users.noreply.github.com>
2020-11-15 22:50:05 +0000

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -14,6 +14,28 @@ do {
 console.log(ans);
 //////////////////////////////////////////////////////////////////////////////////////////
 
+//Question 10
+let i = 1;
+let primeTally = BigInt(1);
+let tally = [];
+let isPrime = true;
+
+do {
+    for (let k = 1; k <= i / 2; k++) {
+        if (tally.length >= 2) {
+            isPrime = false;
+            break;
+        } else if (i % k === 0) {
+            tally.push(k);
+        }
+    }
+    isPrime ? (primeTally += BigInt(i)) : (isPrime = true);
+    tally.length = 0;
+    i += 2;
+} while (i < 2000000);
+console.log(primeTally);
+/////////////////////////////////////////////////////////////////////////////////////////
+
 //Quetion 16
 const sum = BigInt(Math.pow(2, 1000))
     .toString()
