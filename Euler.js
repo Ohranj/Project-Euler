ac574e9e2afb4b67acfc98127401bf23eebe1a86
Ohranj <67558167+Ohranj@users.noreply.github.com>
2021-03-24 20:52:52 +0000

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -132,3 +132,16 @@ for (let i = 1; i <= 1001 * 1001; i += patternCollect) {
 
 console.log(sum);
 ////////////////////////////////////////////////////////////////////////////////////////////
+
+//Question 29 - Distinct Powers
+const allVals = [];
+
+for (let i = 2; i <= 100; i++) {
+    for (let k = 2; k <= 100; k++) {
+	allVals.push(Math.pow(i, k));
+    }
+}
+
+const uniqueVals = [...new Set(allVals)].length;
+console.log(uniqueVals);
+////////////////////////////////////////////////////////////////////////////////////////////
