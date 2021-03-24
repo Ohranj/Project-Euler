c423ded83a2e56d29b3f879c3576db162f3ea43a
Ohranj <67558167+Ohranj@users.noreply.github.com>
2021-03-24 21:45:47 +0000

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -72,6 +72,27 @@ for (let i = 0; factors.length <= 501; i++) {
 console.log(triNum);
 /////////////////////////////////////////////////////////////////////////////////////////
 
+//Question 14 - Longest collatz sequence
+const sequenceVals = [];
+let countTally = 0;
+let ans = 0;
+
+for (let i = 999999; i >= 13; i--) {
+    sequenceVals.length = 0;
+    let val = i;
+    sequenceVals.push(val);
+    do {
+	val % 2 !== 0 ? (val = 3 * val + 1) : (val = val / 2);
+	sequenceVals.push(val);
+    } while (val > 1);
+    if (sequenceVals.length > countTally) {
+	countTally = sequenceVals.length;
+	ans = i;
+    }
+}
+console.log(ans);
+/////////////////////////////////////////////////////////////////////////////////////////
+
 
 //Question 16 - Power digit sum
 const sum = BigInt(Math.pow(2, 1000))
