0128bd6f479268be67f139d4a9dc98ce405cd107
Ohranj <67558167+Ohranj@users.noreply.github.com>
2021-03-24 18:08:48 +0000

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -115,3 +115,20 @@ fib(() => {
     console.log(f.length - 1);
 });
 //////////////////////////////////////////////////////////////////////////////////////////
+
+//Question 28 - Number spiral diagonals
+let patternCollect = 2;
+let sum = 0;
+let valsPerRing = 0;
+
+for (let i = 1; i <= 1001 * 1001; i += patternCollect) {
+    if (valsPerRing === 4) {
+	valsPerRing = 0;
+	patternCollect += 2;
+    }
+    valsPerRing++;
+    sum += i;
+}
+
+console.log(sum);
+////////////////////////////////////////////////////////////////////////////////////////////
