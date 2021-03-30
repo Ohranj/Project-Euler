f48962d3e7b7da40ccfa7c1ded6bd4fe21e5de88
Ohranj <67558167+Ohranj@users.noreply.github.com>
2021-03-30 20:01:52 +0100

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -166,3 +166,17 @@ for (let i = 2; i <= 100; i++) {
 const uniqueVals = [...new Set(allVals)].length;
 console.log(uniqueVals);
 ////////////////////////////////////////////////////////////////////////////////////////////
+
+//Question 30 - Digit fifth powers
+const maxVal = Math.pow(9, 5) * 9;
+let fithPowersSum = 0;
+
+for (let i = 2; i <= maxVal; i++) {
+    const PowerOfDigits = i
+	.toString()
+	.split("")
+	.reduce((acc, c) => (acc += Math.pow(c, 5)), 0);
+    i === PowerOfDigits ? (fithPowersSum += i) : false;
+}
+console.log(fithPowersSum);
+////////////////////////////////////////////////////////////////////////////////////////////
