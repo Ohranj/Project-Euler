2abb3a1bc35d52725271fb9571a19e61b550d2e3
Ohranj <67558167+Ohranj@users.noreply.github.com>
2021-04-27 19:11:45 +0100

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -212,3 +212,21 @@ for (let i = 2; i <= maxVal; i++) {
 }
 console.log(fithPowersSum);
 ////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Question 34 - Digit Factorials
+let sumOfAll = 0;
+for (let i = 3; i <= 362880; i++) {
+    let tallyOfI = 0;
+    for (let k of i.toString()) {
+	let innerTally = 1;
+	for (let j = k; j > 0; j--) {
+	    innerTally *= j;
+	}
+	tallyOfI += innerTally;
+	innerTally = 0;
+    }
+    tallyOfI === i ? (sumOfAll += i) : false;
+}
+console.log(sumOfAll);
+////////////////////////////////////////////////////////////////////////////////////////////
