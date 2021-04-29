c04c1fbbb924ec9955a38ef91415dd346bb6e9a7
Ohranj <67558167+Ohranj@users.noreply.github.com>
2021-04-29 14:27:26 +0100

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -230,3 +230,24 @@ for (let i = 3; i <= 362880; i++) {
 }
 console.log(sumOfAll);
 ////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Question 36 - Double-base palindromes
+let tally = 0;
+
+for (let i = 1; i < 1000000; i++) {
+    const asStr = i.toString();
+    if (asStr[0] != asStr[asStr.length - 1]) {
+	continue;
+    }
+    const asDecimal = Number(i.toString(10).split("").reverse().join(""));
+    const asBinary = i.toString(2).split("").reverse().join("");
+    i === asDecimal
+	? i.toString(2) === asBinary
+	    ? (tally += i)
+	    : false
+        : false;
+}
+
+console.log(tally);
+////////////////////////////////////////////////////////////////////////////////////////////
