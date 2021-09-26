a88da3a99b8610fc854e5370d994c35beb97ee5c
Ohranj <67558167+Ohranj@users.noreply.github.com>
2021-09-26 14:03:30 +0100

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -136,6 +136,29 @@ const getFirst100 = new Promise((res) => {
 /////////////////////////////////////////////////////////////////////////////////////////
 
 
+//Question 19 - Counting Sundays
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 20, 31, 30, 31];
+let sundays = 1;
+let monthStartPos = 1;
+
+for (let i = 0; i <= 100; i++) {
+    for (let k = 0; k < 12; k++) {
+        do {
+	    if (monthStartPos === 1) {
+	        sundays++;
+	    }
+	    monthStartPos += 7;
+	} while (monthStartPos < months[k]);
+	if (monthStartPos > months[k]) {
+	    const nextMonthFirstMon = monthStartPos - months[k];
+	    monthStartPos = nextMonthFirstMon;
+	}
+    }
+}
+console.log(sundays);
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
 //Question 20 - Factorial digit sum
 let i = BigInt(100);
 
