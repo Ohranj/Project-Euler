e816675bf0987d45d778bb86c4fac22aaaa446c6
Ohranj <67558167+Ohranj@users.noreply.github.com>
2021-10-06 18:24:28 +0100

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -266,6 +266,37 @@ console.log(fithPowersSum);
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//Question 32 - Pandigital Primes
+let tally = 0;
+const vals = [];
+
+for (let i = 1; i <= 9876 / 2; i++) {
+    for (let k = 1; k < i / 2; k++) {
+        const val = k * i;
+	const strToArr = [...(String(val) + String(k) + String(i))];
+	if (strToArr.length == 9) {
+	    for (let j = 1; j <= 9; j++) {
+		const indx = strToArr.indexOf(String(j));
+		if (indx >= 0) {
+		    strToArr.splice(indx, 1);
+		} else {
+		    break;
+		}
+	    }
+	    if (!strToArr.length) {
+		if (vals.indexOf(val) === -1) {
+		    vals.push(val);
+		    tally += val;
+		}
+	    }
+	}
+    }
+}
+
+console.log(tally);
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
 //Question 34 - Digit Factorials
 let sumOfAll = 0;
 for (let i = 3; i <= 362880; i++) {
