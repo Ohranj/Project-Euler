f04b8a9bd4020fb2c90c52ba63935e77c2e8accb
Ohranj <67558167+Ohranj@users.noreply.github.com>
2021-10-06 20:43:12 +0100

    Update Euler.js

--- Euler.js
+++ Euler.js
@@ -336,6 +336,23 @@ console.log(tally);
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//Question 40 - Champernowne's constant
+let longStr = "";
+let ans = 1;
+let refPoint = 1;
+
+for (let i = 0; i <= 1000000; i++) {
+    longStr += i;
+    if (i / 10 === refPoint) {
+	refPoint *= 10;
+	ans *= longStr.split("")[i];
+    }
+}
+
+console.log(ans);
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
 //Question 41 - Pandigital Prime
 let primes = 0;
 let isPan = true;
